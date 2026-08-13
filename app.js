dd305a25b250498aa383f22f9f4d82a338eaacfd
jaquin377 <miguelbatistelaewfa@gmail.com>
2026-08-13 09:10:35 -0300

    criamos 3 personagens

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
- const guerreiro = {
- nome: "Thorin",
- vida: 60,
- ataque: 20,
+   class Personagem {
+       constructor (nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
+       }
 
- causaDano: function(personagem){
- personagem.recebeDano(this.ataque)
+       causaDano(personagem){
+        personagem.recebeDano(this.ataque)
+       }
+       recebeDano(quantidade){
+        this.vida = this.vida - quantidade 
+
+        if(this.vida <= 0){
+            this.vida = "eliminado"
+        }
+       }
+
+    
+    
    }
 
-   
-}
 
-const mago = {
-nome: "Gandalfe",
-vida: 60,
-ataque: 35,
+   const druida = new Personagem ("Kode", 80, 25)
 
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
-  }
-}
+const guerreiro = new Personagem("Thorin", 60, 20)
 
-const arqueiro = {
-nome: "Legolas",
-vida: 80,
-ataque: 25
+const mago = new Personagem("Gandalfe", 60, 35)
 
-}
+const arqueiro = new Personagem("Legolas", 80, 25)
+
+ const assasino = new Personagem ("Darius", 70, 35)
+
+   const atirador = new Personagem ("Lyra", 60, 30)
+
+   const cavaleiro = new Personagem ("Zephyr", 50, 50)
+
 guerreiro.causaDano(mago)
+arqueiro.causaDano(mago)
+guerreiro.causaDano(mago)
+assasino.causaDano(atirador)
+atirador.causaDano(cavaleiro)
+cavaleiro.causaDano(assasino)
+
+console.log(assasino.vida)
+console.log(atirador.vida)
+console.log(cavaleiro.vida)
 console.log(mago.vida)
+   
