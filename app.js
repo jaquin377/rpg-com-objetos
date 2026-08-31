dba852bba4a41c3a282188eb0db8f2396137ce00
Jaquin377 <miguelbatistelaewfa@gmail.com>
2026-08-31 08:37:27 -0300

    novas regras

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
    class Personagem {
-       constructor (nome, vida, ataque){
+       constructor (nome, vida, ataque, defesa){
         this.nome = nome,
         this.vida = vida,
-        this.ataque = ataque
+        this.ataque = ataque,
+        this.defesa = defesa
+       }
+       curar(quantidade){
+         this.vida + quantidade
        }
 
        causaDano(personagem){
-        personagem.recebeDano(this.ataque)
+         personagem.recebeDano(this.ataque)
        }
+
        recebeDano(quantidade){
-        this.vida = this.vida - quantidade 
+         let dano = quantidade - this.defesa
 
         if(this.vida <= 0){
             this.vida = "eliminado"
         }
+
+                this.vida = this.vida - quantidade 
+                if (this.vida <= 0)
+                  this.vida = "eliminado"
        }
 
     estaVivo(){
@@ -23,21 +32,21 @@ mostrarStatus(){
 console.log(this.nome + " | Vida: " + this.vida)
 }
     
-   }
+}
 
-   const druida = new Personagem ("Kode", 80, 25)
+   const druida = new Personagem ("Kode", 80, 25, 8)
 
-const guerreiro = new Personagem("Thorin", 60, 20)
+const guerreiro = new Personagem("Thorin", 60, 20, 15)
 
-const mago = new Personagem("Gandalfe", 60, 35)
+const mago = new Personagem("Gandalfe", 60, 35, 5)
 
-const arqueiro = new Personagem("Legolas", 80, 25)
+const arqueiro = new Personagem("Legolas", 80, 25, 15)
 
- const assasino = new Personagem ("Darius", 70, 35)
+ const assasino = new Personagem ("Darius", 70, 35, 10)
 
-   const atirador = new Personagem ("Lyra", 60, 30)
+   const atirador = new Personagem ("Lyra", 60, 30, 6)
 
-   const cavaleiro = new Personagem ("Zephyr", 50, 50)
+   const cavaleiro = new Personagem ("Zephyr", 50, 50, 9)
 
 guerreiro.causaDano(mago)
 arqueiro.causaDano(mago)
